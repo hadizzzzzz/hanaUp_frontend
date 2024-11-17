6f7f36024e243f9da2677d07add3dd9a11821f6f
yminjuu <ymj0580@naver.com>
2024-11-17 17:19:27 +0900

    feat: 나라 icon 유형별 이미지import

--- hanaUp/src/Recoil/travelState.js
+++ hanaUp/src/Recoil/travelState.js
@@ -6,6 +6,6 @@ const { persistAtom } = recoilPersist();
 // before, during, after
 export const travelInfo = atom({
   key: 'travelInfo',
-  default: 'during',
+  default: 'before',
   effects_UNSTABLE: [persistAtom],
 });
